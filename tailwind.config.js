4335e65a500e8739bf2e0f56104a70f9748157c6
pablo <jimeviki@gmail.com>
2024-04-17 17:03:59 -0500

    segundo commit para continuar desde el hogar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0f172a',
+      }
+    },
   },
   plugins: [],
 }
