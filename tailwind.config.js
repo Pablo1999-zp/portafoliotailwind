266001e213b6a1d3e4fe5ef7fb7e5e17618aa194
pablo <jimeviki@gmail.com>
2024-05-19 14:34:41 -0500

    Mi segundo Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const dafaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         primary: '#0f172a',
+      },
+      fontFamily:{
+        roboto:['Roboto', ...defaultTheme.fontFamily.sans]
       }
     },
   },
